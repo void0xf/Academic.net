14ef469194b74b9f14fbfc635c4686dc84d54400
void0xf <150953141+void0xf@users.noreply.github.com>
2025-06-25 22:17:04 +0200

    Add MappingProfile class for AutoMapper configurations to map between domain models and ViewModels

---
+++ WebAppActions/Models/Mapping/MappingProfile.cs
@@ -0,0 +1,25 @@
+using AutoMapper;
+using WebAppActions.Models.ViewModels;
+
+namespace WebAppActions.Models.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Book, BookViewModel>()
+                .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name));
+
+            CreateMap<Book, BookFormViewModel>().ReverseMap();
+
+            CreateMap<Book, BookDetailsViewModel>()
+                .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name))
+                .ForMember(dest => dest.Owners, opt => opt.MapFrom(src => src.UserBooks))
+                .ForMember(dest => dest.Reviews, opt => opt.MapFrom(src => src.Reviews));
+
+            CreateMap<Review, ReviewViewModel>();
+            CreateMap<UserBook, BookOwnerViewModel>()
+                .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.User.Username));
+        }
+    }
+} 
